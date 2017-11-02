3223f6f3ea4482057308d13a32ed513891313462
Timothy Johnson <pavementii@gmail.com>
2017-11-02 10:19:05 -0400

    extended challenge update

--- Lab_8_BattingStatistics.java
+++ Lab_8_BattingStatistics.java
@@ -20,94 +20,110 @@ public class Lab_8_BattingStatistics {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		boolean repeat = true;
+		int playerCounter = 0;
+		int playerCount = Validator.getInt(sc, "How many players? ", 1, 5);
+
+		double[][] playerStorage = new double[playerCount][2];
+
 		/*
 		 * double battingAverage = 0.0; double sluggingPercentage = 0.0;
 		 */
 		ArrayList<Integer> battingData = new ArrayList<Integer>();// use array to store the at-bat results for each
 																	// player
 
-		while(repeat) {
-		int numberOfAtBats = Validator.getInt(sc, "How many at-bats: ");
-		// int numberOfBasesEarned = -1;
+		while (repeat) {
+			for (int player = 0; player < playerCount; player++) {
+				int numberOfAtBats = Validator.getInt(sc, "\nHow many at-bats for player " + (player + 1) + ": ");
 
-		System.out.println("0 = out, 1 = single, 2 = double, 3 = triple, 4 = home run");
-		for (int i = 0; i < numberOfAtBats; i++) {
-			battingData.add(Validator.getInt(sc, "Result for at-bat " + i + ": ", 0, 4));
-		}
-		
-		// System.out.println("\nbattingData: "+ battingData);
-		//System.out.println("battingData size: "+ battingData.size() + "\n");
-		
-		/*
-		 * battingAverage = calculateBattingAverage(battingData); sluggingPercentage =
-		 * calculateSluggingPercentage(battingData);
-		 * 
-		 * System.out.println("Batting average:" + battingAverage);
-		 * System.out.println("Slugging Percentage:" + sluggingPercentage);
-		 */
+				System.out.println("0 = out, 1 = single, 2 = double, 3 = triple, 4 = home run");
+				for (int i = 0; i < numberOfAtBats; i++) {
+					battingData.add(Validator.getInt(sc, "Result for at-bat " + i + ": ", 0, 4));
+					storePlayerData(player, calculateBattingAverage(battingData),
+							calculateSluggingPercentage(battingData), playerStorage);
+				}
 
-		System.out.print("\nBatting average:  "); System.out.printf("%.3f",calculateBattingAverage(battingData));
-		System.out.print("\nSlugging Percentage: "); System.out.printf("%.3f",calculateSluggingPercentage(battingData));
-		System.out.println("\n");
+			/*	storePlayerData(playerCounter, calculateBattingAverage(battingData),
+						calculateSluggingPercentage(battingData), playerStorage);
+			*/	
+			//	battingData.clear();
+			/*	System.out.print("\nBatting average:  ");
+				System.out.printf("%.3f", calculateBattingAverage(battingData));
+				System.out.print("\nSlugging Percentage: ");
+				System.out.printf("%.3f", calculateSluggingPercentage(battingData));
+				System.out.println("\n");*/
 
-		// validate the input so that the user can only enter positive integers for the
-		// at-bat results, the suer can only enter 0,1,2, 3, or 4
-		// validate the users' response to the question "Another batter?" so that the
-		// user can only enter "Y, y, N, or n". No or n, ends the program
-		// format the batting average and slugging percentages such that three decimal
-		// places are shown
+				}
+				
+				
+				
+				for (int i = 0; i < playerStorage.length; i++) {
+					System.out.println("\n\nPlayer " + (i + 1));
+					System.out.print("Batting average: ");
+					System.out.printf("%.3f", playerStorage[i][0]);
+					System.out.print("\nSlugging Percentage: ");
+					System.out.printf("%.3f", playerStorage[i][1]);
+			}
 
-		repeat = repeatYesNo(Validator.getString(sc, "\nAnother batter, yo? "));
-		}
+			repeat = repeatYesNo(Validator.getString(sc, "\n\nAnother batter, yo? "));
+
+	
+		} System.out.println("Okay thanks! Good bye!");
 		sc.close();
 	}
 
 	/**
 	 * @param battingData
 	 */
-	
+
+	public static void storePlayerData(int playerNumber, double battingAverage, double sluggingPercentage,
+			double[][] playerStorage) {
+		playerStorage[playerNumber][0] = battingAverage;
+		playerStorage[playerNumber][1] = sluggingPercentage;
+	}
+
 	private static boolean repeatYesNo(String repeat) {
-		if(repeat.equalsIgnoreCase("Y")) {
+		if (repeat.equalsIgnoreCase("Y")) {
 			return true;
 		}
-		
+
 		return false;
 	}
+
 	private static double calculateSluggingPercentage(ArrayList<Integer> battingData) {
-		//System.out.println("\ncalculateSluggingPercentage()...");
-		
+		// System.out.println("\ncalculateSluggingPercentage()...");
+
 		double sluggingPercentage = 0.0;
 		int totalBasesEarned = 0;
-		
-		for(int bats: battingData) {
-			if(bats > 0 ) {
+
+		for (int bats : battingData) {
+			if (bats > 0) {
 				totalBasesEarned += (int) bats;
-				//System.out.println("totalBasesEarned: " + totalBasesEarned);
+				// System.out.println("totalBasesEarned: " + totalBasesEarned);
 			}
-			
+
 		}
-		
-		//System.out.println("totalBasesEarned: " + totalBasesEarned);
+
+		// System.out.println("totalBasesEarned: " + totalBasesEarned);
 		sluggingPercentage = (double) totalBasesEarned / battingData.size();
-		
+
 		return sluggingPercentage;
 	}
 
 	private static double calculateBattingAverage(ArrayList<Integer> battingData) {
-		//System.out.println("\ncalculateBattingAverage()...");
+		// System.out.println("\ncalculateBattingAverage()...");
 		double battingAverage;
 		int counter = 0;
 
 		for (int bats : battingData) {
 			if (bats > 0) {
 				counter++;
-				//System.out.println("counter: " + counter);
+				// System.out.println("counter: " + counter);
 			}
 		}
-		
+
 		battingAverage = (double) counter / battingData.size();
-		
-		//System.out.println("battingAverage: " + battingAverage);
+
+		// System.out.println("battingAverage: " + battingAverage);
 
 		return battingAverage;
 	}
